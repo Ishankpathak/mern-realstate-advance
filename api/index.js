91133131d0655bc58a737bf39c7822472f47ed51
Ishankpathak <pathakishank4@gmail.com>
2024-04-05 18:39:58 +0530

    sign up ui

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
@@ -13,7 +14,7 @@ mongoose
     console.log(err);
   });
 const app = express();
-
+app.use(express.json());
 // app.get("/test", (req, res) => {
 //   res.json({
 //     message: "Hello, world!",
@@ -25,3 +26,14 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
