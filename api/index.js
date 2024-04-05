86dc6f8ef327e78e62e8423957cb71eddaff01e5
Ishankpathak <pathakishank4@gmail.com>
2024-04-05 17:04:04 +0530

    Test function created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
+
+// app.get("/test", (req, res) => {
+//   res.json({
+//     message: "Hello, world!",
+//   });
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
